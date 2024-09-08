d40c510ed174de4b888ce5552dc504ce96aac75b
Kika7536 <kika7536.p@gmail.com>
2024-09-08 19:25:32 +0300

    Добавьте файлы проекта.

---
+++ w.p.3.5/консоля.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int a, b;
+
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	cout << "-----Проверяем-----" << endl;
+
+	if (a % b == 0) {
+		cout << "Да, " << a << " делится на " << b << " без остатка!" << endl;
+	}
+	else {
+		cout << "Нет, " << a << " не делится на " << b << " без остатка!" << endl;
+	}
+}
